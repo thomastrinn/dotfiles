519311226a59e8bf63a4e8653d6f6b4c42ce3f19
thomas <thomas.trinn@gmail.com>
2020-12-30 19:02:51 +0100

    polybar config: add logout module

---
+++ .config/polybar/modules/logout.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+ANS=$(echo "Lock|Logout|Reboot|Shutdown" | \
+    rofi -sep "|" -dmenu -i -p 'System ' "" -width 20 \
+    -hide-scrollbar -eh 1 -line-padding 4 -padding 5 -lines 4)
+case "$ANS" in
+    *Lock) lockscreen -- scrot ;;
+    *Logout) loginctl terminate-session $(loginctl session-status | head -n 1 | awk '{print $1}') ;;
+    *Reboot) systemctl reboot ;;
+    *Shutdown) systemctl poweroff
