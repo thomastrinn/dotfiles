c07caa81537492fe910fc5d89731ea5638579ef8
thomastrinn <thomas.trinn@gmail.com>
2021-02-03 10:10:54 +0100

    rofi: app launch and switcher config

--- .bin/app-launcher.sh
+++ .bin/app-launcher.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rofi -modi 'window,run,ssh,drun' -show-icons -show drun
+rofi -modi 'drun,window,windowcd' -display-drun 'Launch' -display-window 'Apps' -display-windowcd 'Apps on screen' -sidebar-mode -show-icons -show drun
