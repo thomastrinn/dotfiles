523021d98c6158fd43db92d2b3b746c27b6153e7
thomastrinn <thomas.trinn@gmail.com>
2021-02-24 20:41:09 +0100

    polybar: hlwm-workspaces refactor

--- .config/polybar/scripts/hlwm-workspaces.sh
+++ .config/polybar/scripts/hlwm-workspaces.sh
@@ -23,6 +23,46 @@ not_empthy_tag_fg='#D8DEE9'
 ###############################################################################
 # format tags
 ###############################################################################
+
+format_tag() {
+    tag_status=$1
+    tag_name=$2
+
+    echo "%{B${default_bg}}%{F${default_fg}}"
+    case ${tag_status} in
+        '#')
+			# # the tag is viewed on the specified MONITOR and it is focused.
+            echo "%{B${selected_tag_bg}}%{F${selected_tag_fg}}"
+	        ;;
+		'+')
+            # + the tag is viewed on the specified MONITOR, but this monitor is not focused.
+            echo "%{B${selected_tag_bg}}%{F${selected_tag_fg}}"
+            ;;
+        '-')
+		    # - the tag is viewed on a different MONITOR, but this monitor is not focused.
+            echo "%{B${active_tag_bg}}%{F${active_tag_fg}}"
+		    ;;
+		'%')
+			# % the tag is viewed on a different MONITOR and it is focused.
+            echo "%{B${active_tag_bg}}%{F${active_tag_fg}}"
+			;;
+        ':')
+            # : the tag is not empty
+            echo "%{B${not_empthy_tag_bg}}%{F${not_empthy_tag_fg}}"
+			;;
+        '!')
+            # ! the tag contains an urgent window
+			echo "%{B${urgent_tag_bg}}%{F${urgent_tag_fg}}"
+            ;;
+        *)
+            # . the tag is empty
+            # echo "%{F-}%{B-}" # Add your formatting tags for empty workspaces
+            ;;
+    esac
+
+    echo "%{A1:herbstclient chain , focus_monitor $selected_monitor , use ${i:1}:} ${i:1} %{A -u -o F- B-}"
+}
+
 herbstclient --idle "tag_*" 2>/dev/null | {
 
     while true; do
@@ -31,42 +71,10 @@ herbstclient --idle "tag_*" 2>/dev/null | {
         {
             for i in "${tags[@]}" ; do
                 # Read the prefix from each tag and render them according to that prefix
-                echo "%{B${default_bg}}%{F${default_fg}}"
-                case ${i:0:1} in
-                    '#')
-			            # # the tag is viewed on the specified MONITOR and it is focused.
-                        echo "%{B${selected_tag_bg}}%{F${selected_tag_fg}}"
-	                    ;;
-		            '+')
-            			# + the tag is viewed on the specified MONITOR, but this monitor is not focused.
-                        echo "%{B${selected_tag_bg}}%{F${selected_tag_fg}}"
-            			;;
-                    '-')
-		                # - the tag is viewed on a different MONITOR, but this monitor is not focused.
-                        echo "%{B${active_tag_bg}}%{F${active_tag_fg}}"
-		                ;;
-		            '%')
-			            # % the tag is viewed on a different MONITOR and it is focused.
-                        echo "%{B${active_tag_bg}}%{F${active_tag_fg}}"
-			            ;;
-                    ':')
-                        # : the tag is not empty
-                        echo "%{B${not_empthy_tag_bg}}%{F${not_empthy_tag_fg}}"
-			            ;;
-                    '!')
-                        # ! the tag contains an urgent window
-			            echo "%{B${urgent_tag_bg}}%{F${urgent_tag_fg}}"
-                        ;;
-                    *)
-                        # . the tag is empty
-                        # echo "%{F-}%{B-}" # Add your formatting tags for empty workspaces
-                        ;;
-                esac
-
-	        echo "%{A1:herbstclient chain , focus_monitor $selected_monitor , use ${i:1}:} ${i:1} %{A -u -o F- B-}"
+                format_tag ${i:0:1} ${i:1}
             done
 
-	    echo "%{F-}%{B-}"
+	        echo "%{F-}%{B-}" # reset format
         } | tr -d "\n"
 
     echo
