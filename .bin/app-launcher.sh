0f0d92f967ae29a6a33e82c5bcac4021365d582c
thomastrinn <thomas.trinn@gmail.com>
2021-03-28 18:09:01 +0200

    nop

--- .bin/app-launcher.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-rofi -modi 'drun,window,windowcd' -display-drun 'Launch' -display-window 'Apps' -display-windowcd 'Apps on screen' -sidebar-mode -show-icons -show drun
--- .bin/calc.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# Do live calculations with rofi (https://github.com/svenstaro/rofi-calc)
-
-rofi -show calc -modi calc -no-show-match -no-sort
