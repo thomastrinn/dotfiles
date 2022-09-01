d8c17c25f7a90985903f7b4600b0b4102f4ebfd8
thomastrinn <thomas.trinn@gmail.com>
2022-09-01 21:55:29 +0200

    update

--- .config/herbstluftwm/theme.sh
+++ .config/herbstluftwm/theme.sh
@@ -12,9 +12,13 @@ hsetroot -solid '#2E3440'
 #feh --bg-fill ~/Pictures/wave-color-nord.png
 feh --bg-fill ~/Pictures/mountains-1412683.jpg
 
+hc set auto_detect_monitors true
+
 # Theme
+# First reset all values to default
 hc attr theme.tiling.reset 1
 hc attr theme.floating.reset 1
+
 hc set frame_border_active_color '#81A1C1'
 hc set frame_border_normal_color '#2E3440'
 hc set frame_bg_normal_color '#2E3440'
@@ -27,13 +31,13 @@ hc set frame_transparent_width 0
 hc set frame_active_opacity 100
 hc set frame_normal_opacity 0
 hc set frame_gap 0
-hc set show_frame_decorations=focused_if_multiple
+hc set show_frame_decorations if_empty
 
 hc attr theme.active.color '#81A1C1'
 hc attr theme.normal.color '#3B4252'
 hc attr theme.urgent.color '#EBCB8B'
 hc attr theme.inner_width 0
-hc attr theme.inner_color black
+hc attr theme.inner_color '#3B4252'
 hc attr theme.border_width 1
 hc attr theme.floating.border_width 1
 hc attr theme.floating.outer_width 0
@@ -42,10 +46,10 @@ hc attr theme.active.inner_color '#81A1C1'
 hc attr theme.active.outer_color '#81A1C1'
 hc attr theme.background_color '#2E3440'
 
-hc set window_gap 0
+hc set window_gap 6
 hc set frame_padding 0
-hc set smart_window_surroundings off
-hc set smart_frame_surroundings on
+hc set smart_window_surroundings false
+hc set smart_frame_surroundings hide_all
 hc set mouse_recenter_gap 100
-hc set update_dragged_clients 1
+hc set update_dragged_clients true
 
